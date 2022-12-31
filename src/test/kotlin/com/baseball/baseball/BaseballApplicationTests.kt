b74dc1ea65a510fc5a015cbc2bf4f479e1875970
choikangheon <52992334+choikangheon@users.noreply.github.com>
2022-12-31 17:07:33 +0900

    baseball 코틀린&TDD&DDD

---
+++ src/test/kotlin/com/baseball/baseball/BaseballApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.baseball.baseball
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BaseballApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
